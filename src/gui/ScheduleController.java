4959a1512a0c09cca5788c4fd3d4ec65e0b517a3
LiuYY3 <yiming.liu95@gmail.com>
2017-04-17 21:30:04 -0500

    some elements in schedule fxml

---
+++ src/gui/ScheduleController.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+public class ScheduleController {
+	
+	@FXML
+	Button update;
+	
+	@FXML
+	Button check;
+	
+	@FXML
+	Button reset;
+	
+	@FXML
+	DatePicker date;
+	
+	@FXML
+	public void initialize() {
+		
+	}
+}

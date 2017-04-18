e83a19b0a16c322875feebd82fae51b26e6f0d2b
LiuYY3 <yiming.liu95@gmail.com>
2017-04-17 20:59:55 -0500

    Completed dataConstructor
    create table and insert data
    connect to db
    close connection

--- src/database/DBConstructorTest.java
+++ src/database/DBConstructorTest.java
@@ -2,7 +2,11 @@ package database;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//import java.util.ArrayList;
 //import java.util.Arrays;
 
 import org.junit.Before;
@@ -11,29 +15,24 @@ import org.junit.Test;
 public class DBConstructorTest {
 	
 	DBConstructor db;
-	ArrayList<String> fields;
-	ArrayList<String> types;
 	
 	@Before
-	public void setup() {
+	public void setup() throws ClassNotFoundException {
 		db = new DBConstructor();
-		fields = new ArrayList<String>();
-		types = new ArrayList<String>();
+		db.init();
 	}
 	
 	@Test
 	public void test() {
-		fields.add("ID");
-		types.add("integer");
+		String[] fields = {"ID"};
+		String[] types = {"integer"};
 		assertEquals("ID integer", db.buildFields(fields, types));
 	}
 	
 	@Test
 	public void test2() {
-		fields.add("ID");
-		fields.add("Project");
-		types.add("integer");
-		types.add("string");
+		String[] fields = {"ID", "Project"};
+		String[] types = {"integer", "string"};
 		System.out.println(db.buildFields(fields, types));
 		assertEquals("ID integer, Project string", db.buildFields(fields, types));
 	}
@@ -43,4 +42,33 @@ public class DBConstructorTest {
 //		fields = {"1", "2"};
 //		System.out.println(fields);
 //	}
+	
+	@Test
+	public void test3() {
+		String[] data = {"1"};
+		assertEquals("1", db.buildData(data));
+	}
+	
+	@Test
+	public void test4() {
+		String[] data = {"1", "'DataBase'"};
+		assertEquals("1, 'DataBase'", db.buildData(data));
+	}
+	
+	@Test
+	public void test5() throws SQLException {
+		String[] fields = {"ID", "Project"};
+		String[] types = {"integer", "string"};
+		String[] data = {"1", "'GuitarTab'"};
+		String[] data1 = {"2", "'PeerToPeer'"};
+		String[] data2 = {"3", "'DataBase'"};
+		String tablename = "test";
+		Connection con = db.connectDB();
+		Statement stat = db.editDB(con);
+		db.createTable(stat, tablename, fields, types);
+		db.insertData(stat, tablename, data);
+		db.insertData(stat, tablename, data1);
+		db.insertData(stat, tablename, data2);
+		db.closeDB(con);
+	}
 }

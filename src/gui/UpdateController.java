c6a660f845d027dd22600766857cb8bd7431b3ec
LiuYY3 <yiming.liu95@gmail.com>
2017-04-18 20:11:48 -0500

    beginning of updatecontroller

--- src/gui/UpdateController.java
+++ src/gui/UpdateController.java
@@ -1,5 +1,22 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
 public class UpdateController {
+	
+	@FXML
+	Button add;
+	
+	@FXML
+	TextArea des;
+	
+	@FXML
+	DatePicker date;
+	
+	@FXML
+	
 
 }

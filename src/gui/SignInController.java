aa0798199ac12d7565c84f77b13f8c95cd2f7544
MasonMillsap <millsapms@hendrix.edu>
2017-04-20 11:55:03 -0500

    Sign in works. New DBBuilder class
    Sign in/create works but the checking to see if a username already
    exists isn't implemented correctly. Creating new username creates new
    table in database

--- src/gui/SignInController.java
+++ src/gui/SignInController.java
@@ -1,8 +1,19 @@
 package gui;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.DBBuilder;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class SignInController {
 	
@@ -15,26 +26,57 @@ public class SignInController {
 	@FXML
 	TextField createUsername;
 	
+	DBBuilder dbBuilder;
+	
+	private ArrayList<String> usernames;
+	
 	@FXML
-	public void initialize() {
-		
+	public void initialize() throws ClassNotFoundException, SQLException {
+		dbBuilder = new DBBuilder();
+		usernames = new ArrayList<String>();
 	}
 	
-	/* Check the database that the name exists. Set the static variable ScheduleController.USERNAME to input text. Then, load the correct
-	 * schedule.
-	 */
 	
 	@FXML
 	public void signIn() {
-		
+		if (usernames.contains(enterUsername.getText())) {
+			ScheduleController.USERNAME = createUsername.getText();
+			loadNext();
+		} else {
+			Alert alert = new Alert(AlertType.ERROR, "Username does not exist. Create a new one!", ButtonType.OK);
+			alert.showAndWait();
+		}
 	}
 	
-	/* When a user creates a profile, check the database to verify the name does not exist. Then, add a new record with the input name 
-	 * to each of the 5 tables.
-	 */
+
 	@FXML
-	public void create() {
-		
+	public void create() throws SQLException {
+		String newuser = new String();
+		newuser = createUsername.getText();
+		if (!usernames.contains(newuser)) {
+			usernames.add(newuser);
+			dbBuilder.addTable(newuser);
+			ScheduleController.USERNAME = newuser;
+			loadNext();
+		} else {
+			Alert alert = new Alert(AlertType.ERROR, "Username already exists. Sign in!", ButtonType.OK);
+			alert.showAndWait();
+		}
+	}
+	
+	private void loadNext() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("ScheduleGUI.fxml"));
+			TabPane root = (TabPane) loader.load();
+			Stage secondStage = new Stage();
+			Scene scene = new Scene(root);
+			secondStage.setScene(scene);
+			secondStage.show();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		create.getScene().getWindow().hide();
 	}
 
 }

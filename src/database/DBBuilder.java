06d3e273099482beef1f7ece697399d453ae1eb6
MasonMillsap <millsapms@hendrix.edu>
2017-04-30 11:00:40 -0500

    Reverting some changes that were lost in a sync

--- src/database/DBBuilder.java
+++ src/database/DBBuilder.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import gui.ScheduleController;
 
@@ -19,12 +20,29 @@ public class DBBuilder {
 
 	public void addTable(String username) throws SQLException {
 		 openConStat();
+		 ArrayList<String> times = buildTimeArray();
 		 stat.execute("CREATE TABLE " + username + " (Time TEXT, Monday TEXT, Tuesday TEXT, Wednesday TEXT, Thursday TEXT, Friday TEXT)");
-		 for (int time = 8; time < 23; time++) {
-			 stat.execute("INSERT INTO " + username + " (Time) VALUES ("+ Integer.toString(time) + ")");
+		 for (String time : times) {
+			 stat.execute("INSERT INTO " + username + " VALUES ('"+ time + "', 'FREE', 'FREE', 'FREE', 'FREE', 'FREE')");
 		 }
 		 con.close();
 	}
+	
+	private ArrayList<String> buildTimeArray() {
+		ArrayList<String> times = new ArrayList<String>();
+		for (int time = 8; time < 12; time ++) {
+			times.add(Integer.toString(time) + ":00 AM");
+			times.add(Integer.toString(time) + ":30 AM");
+		}
+		times.add("12:00 PM");
+		times.add("12:30 PM");
+		for (int time = 1; time < 11; time ++) {
+			times.add(Integer.toString(time) + ":00 PM");
+			times.add(Integer.toString(time) + ":30 PM");
+		}
+		return times;
+	}
+	 
 
 	private void openConStat() throws SQLException {
 		con = DriverManager.getConnection("jdbc:sqlite:project4.db");
@@ -38,18 +56,21 @@ public class DBBuilder {
 		con.close();
 	}
 
-	// Figure out how to do this from here:
-	// http://stackoverflow.com/questions/2942788/check-if-table-exists
+
 	public boolean isTable(String username) throws SQLException {
+		boolean isTable;
 		openConStat();
 		DatabaseMetaData soMetaBro = con.getMetaData();
 		ResultSet tables = soMetaBro.getTables(null, null, username, null);
-		con.close();
+
 		if (tables.next()) {
-			return true;
+			isTable = true;
 		} else {
-			return false;
+			isTable = false;
 		}
+
+		con.close();
+		return isTable;
 	}
 
 	public void removeTable(String username) throws SQLException {
@@ -57,12 +78,13 @@ public class DBBuilder {
 		stat.executeQuery("DROP TABLE IF EXISTS " + username + ";");
 		con.close();
 	}
-
+	
 	public String isFree(String query, String day) throws SQLException {
 		openConStat();
 		stat.execute(query);
 		ResultSet rs = stat.getResultSet();
 		String free = rs.getString(day);
+		con.close();
 		return free;
 	}
 }
